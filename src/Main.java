e032e026218388eafdacd87ab77d8e650d0a0359
Nicolas Pedranti <nicopedranti@gmail.com>
2025-10-19 18:37:16 -0300

    feat: implement categories menu and integrate with CategoryService in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,25 @@
 public class Main {
 
   public static void main(String[] args) {
-    final ItemService itemService = new ItemService(new ItemRepository());
+    final ItemRepository itemRepository = new ItemRepository();
+    final CategoryRepository categoryRepository = new CategoryRepository();
+
+    final CategoryService categoryService = new CategoryService(categoryRepository, itemRepository);
+    final ItemService itemService = new ItemService(itemRepository, categoryService);
 
     int option;
 
     do {
       System.out.println("\n=== Menú Principal ===");
       System.out.println("1) CRUD de Productos");
-      // System.out.println("2) CRUD de Categorías");
+      System.out.println("2) CRUD de Categorías");
       System.out.println("0) Salir");
 
       option = ConsoleInput.readInt("Opción: ");
 
       switch (option) {
         case 1 -> productMenu(itemService);
+        case 2 -> categoriesMenu(categoryService);
         case 0 -> System.out.println("¡Hasta luego!");
         default -> System.out.println("Opción inválida");
       }
@@ -40,6 +45,31 @@ public class Main {
         case 3 -> service.getByName();
         case 4 -> service.updateItem();
         case 5 -> service.deleteItem();
+        case 0 -> System.out.println("Volviendo al menú principal...");
+        default -> System.out.println("Opción inválida");
+      }
+    } while (option != 0);
+  }
+
+  private static void categoriesMenu(CategoryService service) {
+    int option;
+    do {
+      System.out.println("\n--- CRUD Categorías ---");
+      System.out.println("1) Crear");
+      System.out.println("2) Listar");
+      System.out.println("3) Buscar por nombre");
+      System.out.println("4) Actualizar");
+      System.out.println("5) Eliminar");
+      System.out.println("0) Volver");
+
+      option = ConsoleInput.readInt("Opción: ");
+
+      switch (option) {
+        case 1 -> service.createCategory();
+        case 2 -> service.getAllCategories();
+        case 3 -> service.getByName();
+        case 4 -> service.updateCategory();
+        case 5 -> service.deleteCategory();
         case 0 -> System.out.println("Volviendo al menú principal...");
         default -> System.out.println("Opción inválida");
       }
