32ef90a1a5604df55092d9d5d1054a54da3b30ef
Nicolas Pedranti <nicopedranti@gmail.com>
2025-10-18 21:33:19 -0300

    refactor: rename BaseEntity to Item to generalize for products and services

--- src/Item.java
+++ src/Item.java
@@ -1,11 +1,15 @@
-public abstract class BaseEntity {
+public abstract class Item {
   private int id;
   private String name;
+  private double price;
   private boolean isDeleted = false;
 
-  protected BaseEntity(int id, String name) {
-    this.id = id;
+  private static int nextId = 1;
+
+  protected Item(String name, double price) {
+    this.id = nextId++;
     this.name = name;
+    this.price = price;
   }
 
   public int getId() {
@@ -18,6 +22,14 @@ public abstract class BaseEntity {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
   }
 
   public boolean isDeleted() {
